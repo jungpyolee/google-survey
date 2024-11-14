26ead38382306aeeece1591d6535a64fd7aaeca4
jungpyolee <jungpyo5789@gmail.com>
2024-11-14 13:44:38 +0900

    feat: Title component & animation config

---
+++ src/components/Title.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import { setDescription, setTitle } from "../store/surveySlice";
+import TextareaAutosize from "react-textarea-autosize";
+
+const Title: React.FC = () => {
+  const [titleFocused, setTitleFocused] = useState(false);
+  const [descriptionFocused, setDescriptionFocused] = useState(false);
+
+  const dispatch = useDispatch();
+  const title = useSelector((state: RootState) => state.survey.title);
+  const description = useSelector(
+    (state: RootState) => state.survey.description,
+  );
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    dispatch(setTitle(e.target.value));
+  };
+  const handleDescriptionChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    dispatch(setDescription(e.target.value));
+  };
+
+  return (
+    <div className="rounded-lg bg-white shadow-md">
+      <div className="h-3 rounded-t-lg bg-violet-600"></div>
+      <div className="px-8 py-6">
+        <TextareaAutosize
+          className="mt-4 h-auto w-full resize-none text-3xl focus:outline-none focus:ring-0"
+          value={title}
+          minRows={1}
+          maxRows={5}
+          onChange={handleTitleChange}
+          placeholder="설문지 제목"
+          onFocus={() => setTitleFocused(true)}
+          onBlur={() => setTitleFocused(false)}
+        />
+        <div
+          className={`h-0.5 bg-violet-600 ${titleFocused ? "animate-expand" : "bg-gray-200"}`}
+        ></div>
+        <TextareaAutosize
+          className="mt-4 w-full resize-none text-sm focus:outline-none focus:ring-0"
+          value={description}
+          minRows={1}
+          maxRows={5}
+          onChange={handleDescriptionChange}
+          placeholder="설문지 설명"
+          onFocus={() => setDescriptionFocused(true)}
+          onBlur={() => setDescriptionFocused(false)}
+        />
+        <div
+          className={`h-0.5 bg-violet-600 ${descriptionFocused ? "animate-expand" : "bg-gray-200"}`}
+        ></div>
+      </div>
+      {/* 설문지 설명 */}
+    </div>
+  );
+};
+
+export default Title;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        expand: "expand 0.3s ease-in-out", // 애니메이션 이름과 속도 설정
+      },
+      keyframes: {
+        expand: {
+          "0%": { transform: "scaleX(0)" }, // 시작 시 scaleX(0)
+          "100%": { transform: "scaleX(1)" }, // 끝날 때 scaleX(1)로 확장
+        },
+      },
+    },
   },
   plugins: [],
 };
