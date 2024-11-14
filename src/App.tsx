e76792b6cc85d85949f721fbc805b9e60fbcb2bb
jungpyolee <jungpyo5789@gmail.com>
2024-11-14 09:51:28 +0900

    feat: routing for edit, viewform

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Edit from "./pages/Edit";
+import ViewForm from "./pages/Viewform";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div className="App">
-        <h1>Survey</h1>
+        {/* 라우팅 */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/edit" />} />
+            <Route path="/edit" element={<Edit />} />
+            <Route path="/viewform" element={<ViewForm />} />
+          </Routes>
+        </Router>
       </div>
     </Provider>
   );
---
+++ src/pages/Viewform.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ViewForm: React.FC = () => {
+  console.log("hi");
+  return (
+    <div className="h-4 w-4 bg-red-500">
+      <h1>View Form</h1>
+    </div>
+  );
+};
+
+export default ViewForm;
