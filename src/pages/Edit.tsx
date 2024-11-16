5a1b8f6f04f6c968e767d7c20075b28cb30d3b2a
jungpyolee <jungpyo5789@gmail.com>
2024-11-16 17:08:18 +0900

    feat: add Edit Page

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Title from "../components/Title";
+import Questions from "../components/Questions";
+import Sidebar from "../components/Sidebar";
+import Bottombar from "../components/Bottombar";
+import { RemoveRedEye } from "@mui/icons-material";
+import { Button } from "@mui/material";
 
 const Edit: React.FC = () => {
   return (
-    <div>
-      <h1>Edit</h1>
+    <div className="w-full bg-violet-100 p-8 md:mx-auto md:w-1/2">
+      <div className="flex justify-end pb-1">
+        <Button color="secondary" aria-label="미리보기" onClick={() => {}}>
+          <RemoveRedEye />
+          <span className="px-1 text-sm">미리 보기</span>
+        </Button>
+      </div>
+      <div className="flex-1">
+        <Title />
+        <Questions />
+      </div>
+      <Bottombar />
+
+      <div className="fixed bottom-4 right-4 md:bottom-8 md:right-8">
+        <Sidebar />
+      </div>
     </div>
   );
 };
