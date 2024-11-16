ce41b8d863f05d7114a2d3394e8753b311be6cd5
jungpyolee <jungpyo5789@gmail.com>
2024-11-16 17:03:27 +0900

    fix: height error

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <div className="App bg-violet-100">
+        <div className="App min-h-screen bg-violet-100">
           {/* 라우팅 */}
           <Router>
             <Routes>
